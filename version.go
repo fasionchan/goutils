b7cae4c486e27a683cb293c7ec3a625cd2b22f99
Fasion Chan <fasionchan@gmail.com>
2022-11-08 15:16:50 +0800

    first version

---
+++ version.go
@@ -0,0 +1,14 @@
+/*
+ * Author: fasion
+ * Created time: 2022-11-08 15:12:46
+ * Last Modified by: fasion
+ * Last Modified time: 2022-11-08 16:25:52
+ */
+
+package goutils
+
+const version = "v1.0.0"
+
+func Version() string {
+	return version
+}
