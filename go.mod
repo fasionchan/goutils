e201c9d475102d698d50d7106ea9a210e4636e20
Fasion Chan <fasionchan@gmail.com>
2022-11-08 15:16:50 +0800

    first version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fasionchan/goutils
+
+go 1.19
---
+++ version.go
@@ -0,0 +1,14 @@
+/*
+ * Author: fasion
+ * Created time: 2022-11-08 15:12:46
+ * Last Modified by: fasion
+ * Last Modified time: 2022-11-08 15:13:27
+ */
+
+package gotuils
+
+const version = "v0.0.1"
+
+func Version() string {
+	return version
+}
