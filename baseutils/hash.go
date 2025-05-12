9a10c16ed2ee53b235fcd0714a8f5a356cdfc5db
Fasion Chan <fasionchan@gmail.com>
2025-05-12 14:27:58 +0800

    hash helpers

--- baseutils/hash.go
+++ baseutils/hash.go
@@ -2,7 +2,7 @@
  * Author: fasion
  * Created time: 2023-01-12 15:04:53
  * Last Modified by: fasion
- * Last Modified time: 2023-01-12 15:10:31
+ * Last Modified time: 2025-05-12 14:26:26
  */
 
 package baseutils
@@ -13,8 +13,167 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"fmt"
 	"hash"
+
+	"github.com/fasionchan/goutils/stl"
 )
+
+var (
+	md5Hasher    Hasher = md5.New
+	sha1Hasher   Hasher = sha1.New
+	sha224Hasher Hasher = sha256.New224
+	sha256Hasher Hasher = sha256.New
+	sha512Hasher Hasher = sha512.New
+)
+
+// todo: where to move?
+func StringToBytes(s string) []byte {
+	return []byte(s)
+}
+
+func GetMd5Hasher() Hasher {
+	return md5Hasher
+}
+
+func GetSha1Hasher() Hasher {
+	return sha1Hasher
+}
+
+func GetSha224Hasher() Hasher {
+	return sha224Hasher
+}
+
+func GetSha256Hasher() Hasher {
+	return sha256Hasher
+}
+
+func GetSha512Hasher() Hasher {
+	return sha512Hasher
+}
+
+type Hasher func() hash.Hash
+
+func (hasher Hasher) New() hash.Hash {
+	return hasher()
+}
+
+func (hasher Hasher) NewHashPro() *HashPro {
+	return NewHashPro(hasher())
+}
+
+func (hasher Hasher) Sum(data []byte) []byte {
+	h := hasher()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func (hasher Hasher) SumBstob(datas [][]byte) []byte {
+	h := hasher()
+
+	for _, data := range datas {
+		h.Write(data)
+	}
+
+	return h.Sum(nil)
+}
+
+func (hasher Hasher) SumBstobX(datas ...[]byte) []byte {
+	return hasher.SumBstob(datas)
+}
+
+func (hasher Hasher) SumBstos(datas [][]byte) string {
+	return fmt.Sprintf("%x", hasher.SumBstob(datas))
+}
+
+func (hasher Hasher) SumBstosX(datas ...[]byte) string {
+	return hasher.SumBstos(datas)
+}
+
+func (hasher Hasher) SumBtob(data []byte) []byte {
+	return hasher.Sum(data)
+}
+
+func (hasher Hasher) SumBtos(data []byte) string {
+	return fmt.Sprintf("%x", hasher.Sum(data))
+}
+
+func (hasher Hasher) SumSstob(datas []string) []byte {
+	return hasher.SumBstob(stl.Map(datas, StringToBytes))
+}
+
+func (hasher Hasher) SumSstobX(datas ...string) []byte {
+	return hasher.SumBstob(stl.Map(datas, StringToBytes))
+}
+
+func (hasher Hasher) SumSstos(datas []string) string {
+	return hasher.SumBstos(stl.Map(datas, StringToBytes))
+}
+
+func (hasher Hasher) SumSstosX(datas ...string) string {
+	return hasher.SumBstos(stl.Map(datas, StringToBytes))
+}
+
+func (hasher Hasher) SumStob(data string) []byte {
+	return hasher.Sum([]byte(data))
+}
+
+func (hasher Hasher) SumStos(data string) string {
+	return hasher.SumBtos([]byte(data))
+}
+
+func HashSum(hash hash.Hash, data []byte) []byte {
+	return hash.Sum(data)
+}
+
+func HashSumBtob(hash hash.Hash, data []byte) []byte {
+	return hash.Sum(data)
+}
+
+func HashSumBtos(hash hash.Hash, data []byte) string {
+	return fmt.Sprintf("%x", hash.Sum(data))
+}
+
+func HashSumStob(hash hash.Hash, data string) []byte {
+	return hash.Sum([]byte(data))
+}
+
+func HashSumStos(hash hash.Hash, data string) string {
+	return HashSumBtos(hash, []byte(data))
+}
+
+type HashPro struct {
+	hash.Hash
+}
+
+func NewHashPro(hash hash.Hash) *HashPro {
+	return &HashPro{
+		Hash: hash,
+	}
+}
+
+func (hash *HashPro) Native() hash.Hash {
+	if hash == nil {
+		return nil
+	}
+	return hash.Hash
+}
+
+func (hash *HashPro) SumBtob(data []byte) []byte {
+	return HashSumBtob(hash.Hash, data)
+}
+
+func (hash *HashPro) SumBtos(data []byte) string {
+	return HashSumBtos(hash.Hash, data)
+}
+
+func (hash *HashPro) SumStob(data string) []byte {
+	return HashSumStob(hash.Hash, data)
+}
+
+func (hash *HashPro) SumStos(data string) string {
+	return HashSumBtos(hash, []byte(data))
+}
 
 var hasherCreators = map[crypto.Hash]func() hash.Hash{
 	crypto.MD5:    md5.New,
---
+++ baseutils/hash_test.go
@@ -0,0 +1,64 @@
+/*
+ * Author: fasion
+ * Created time: 2024-08-07 17:31:50
+ * Last Modified by: fasion
+ * Last Modified time: 2025-05-12 14:27:02
+ */
+
+package baseutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMd5Hasher(t *testing.T) {
+	hasher := GetMd5Hasher()
+
+	for _, testCase := range []struct {
+		data string
+		sum  string
+	}{
+		{
+			data: "",
+			sum:  "d41d8cd98f00b204e9800998ecf8427e",
+		},
+		{
+			data: "a",
+			sum:  "0cc175b9c0f1b6a831c399e269772661",
+		},
+		{
+			data: "abc",
+			sum:  "900150983cd24fb0d6963f7d28e17f72",
+		},
+	} {
+		sum := hasher.SumStos(testCase.data)
+		assert.Equal(t, testCase.sum, sum, "data: %s", testCase.data)
+	}
+
+	for _, testCase := range []struct {
+		datas []string
+		sum   string
+	}{
+		{
+			datas: nil,
+			sum:   "d41d8cd98f00b204e9800998ecf8427e",
+		},
+		{
+			datas: []string{},
+			sum:   "d41d8cd98f00b204e9800998ecf8427e",
+		},
+		{
+			datas: []string{"a"},
+			sum:   "0cc175b9c0f1b6a831c399e269772661",
+		},
+		{
+			datas: []string{"a", "b", "c"},
+			sum:   "900150983cd24fb0d6963f7d28e17f72",
+		},
+	} {
+		sum := hasher.SumSstos(testCase.datas)
+		assert.Equal(t, testCase.sum, sum, "datas: %v", testCase.datas)
+	}
+}
